8205191175625c87402bec711f5f128977d3397f
kushal pokhrel <71119638+thekushalpokhrel@users.noreply.github.com>
2020-11-21 11:55:46 +1100

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package Kushal180160.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
